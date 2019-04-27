e186cd8bafc750086de397fbfae6e56727562b0d
marcinzaraj <marcinzaraj@gmail.com>
2019-04-27 16:55:27 +0200

    ASCI converter

--- src/main/java/back/Main.java
+++ src/main/java/back/Main.java
@@ -1,7 +1,8 @@
 package back;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("randomowy text");
     }
 }
