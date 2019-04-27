41c20a15c22c0febeed558595af33f1f2a73236a
awesomix <awesomix@interia.eu>
2019-04-27 20:06:41 +0200

    parity - wysylanie updated

--- src/main/java/back/Main.java
+++ src/main/java/back/Main.java
@@ -6,6 +6,6 @@ package back;
         public static void main(String[] args) {
             Gui gui = new Gui();
             gui.openMainWindow();
-            //System.out.println(Parity.wysylanie("00101010"));
+            //System.out.println(Parity.wysylanie("1111111100000001"));
         }
 }
--- src/main/java/back/Parity.java
+++ src/main/java/back/Parity.java
@@ -4,22 +4,50 @@ public class Parity {
     public static String wysylanie(String str)
     {
         int ilosc=0;
-        String newStr;
-        for(int i=0;i<str.length();i++)
+        StringBuilder newStr = new StringBuilder();
+        int start=0;
+        int koniec=8;
+        for(int j=0;j<str.length()/8;j++)
+        {
+            for(int i=start;i<koniec;i++) //zliczanie "1" w danym przedziale ośmiu znaków w stringu
+            {
+                if(String.valueOf(str.charAt(i)).equals("1"))
+                {
+                    ilosc++;
+                }
+            }
+            if(ilosc%2==0) //sprawdzamy czy ilość "1" jest parzysta i dodajemy odpowiedni bit parzystości
+            {
+                newStr.append("0");
+            }
+            else
+            {
+                newStr.append("1");
+            }
+            for(int k=start;k<koniec;k++) //dodajemy wcześniej sprawdzaną część stringa
+            {
+                newStr.append(str.charAt(k));
+            }
+            ilosc=0;
+            start+=8;
+            koniec+=8;
+        }
+        return String.valueOf(newStr);
+    }
+    public static String odbieranie(String str) //work in progress
+    {
+        int ilosc=0;
+        for(int i=0;i<str.length();i++) //zliczanie "1"
         {
             if(String.valueOf(str.charAt(i)).equals("1"))
             {
                 ilosc++;
             }
         }
-        if(ilosc%2==0)
+        if(ilosc%2!=0)
         {
-            newStr="0"+str;
+
         }
-        else
-        {
-            newStr="1"+str;
-        }
-        return newStr;
+        return str;
     }
 }
