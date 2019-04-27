bae7c9b828378db4c3c66845fb6f8de82bd3ee26
awesomix <awesomix@interia.eu>
2019-04-27 21:13:19 +0200

    hamming-dodana klasa, wymaga więcej czytania jednak

---
+++ src/main/java/back/Hamming.java
@@ -0,0 +1,12 @@
+package back;
+
+public class Hamming {
+    public static String wysylanie(String str) // work in progress
+    {
+        return str;
+    }
+    public static String odbieranie(String str)
+    {
+        return str;
+    }
+}
