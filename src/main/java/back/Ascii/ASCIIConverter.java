4b41568230ada23e7060d04784ac944aff0ff523
nika-1993 <nika.1993@interia.pl>
2019-05-12 08:53:17 +0200

    ascii string ->stringbuilder

--- src/main/java/back/Ascii/ASCIIConverter.java
+++ src/main/java/back/Ascii/ASCIIConverter.java
@@ -2,34 +2,31 @@ package back.Ascii;
 
 public class ASCIIConverter {
 
-    public String converterToASCI(String text){
+    public String converterToASCI(String text) {
 
         byte[] bytes = text.getBytes();
         StringBuilder binary = new StringBuilder();
 
         for (byte b : bytes) {
             int val = b;
-            for (int i = 0; i < 8; i++)
-            {
+            for (int i = 0; i < 8; i++) {
                 binary.append((val & 128) == 0 ? 0 : 1);
                 val <<= 1;
             }
         }
-        String convertedText = binary.toString();
-        return convertedText;
+        return binary.toString();
     }
 
-    public String convertedToText( String byteText) {
+    public String convertedToText(String byteText) {
 
-        String text = "";
+        StringBuilder text = new StringBuilder();
         for (int index = 0; index < byteText.length(); index += 8) {
             String temp = byteText.substring(index, index + 8);
             int num = Integer.parseInt(temp, 2);
             char letter = (char) num;
-            text = text + letter;
-
+            text.append(letter);
         }
-        return text;
+        return text.toString();
     }
 
 }
