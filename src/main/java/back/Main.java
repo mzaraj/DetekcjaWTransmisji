13d9bcd4ee72edfa0cdb158231c8352a98acfaae
awesomix <awesomix@interia.eu>
2019-05-13 23:59:27 +0200

    hamming done, this should hopefully probably maybe work

--- src/main/java/back/Main.java
+++ src/main/java/back/Main.java
@@ -8,16 +8,5 @@ public class Main {
     public static void main(String[] args) {
         Gui gui = new Gui();
         gui.openMainWindow();
-//          System.out.println(Hamming.send("101010101010101010101010101010101010101010101010"));
-//          String[] strings = Hamming.receive("001001011000101001011000101001011000101001011000101001011000101001011001");
-//          for(int i=0;i<2;i++)
-//          {
-//              System.out.println(strings[i]);
-//          }
-//          ArrayList<Integer> list = ErrorCount.getDetectedErrorsIndexes();
-//          for(int i=0; i<list.size();i++)
-//          {
-//              System.out.println(list.get(i));
-//          }
     }
 }
